1ba3dd4fbb72af0ab98732095979985e6ee5bcd3
sunbohong <sunbohong@gmail.com>
2014-12-01 10:56:08 +0800

    Initial Commit

---
+++ TextViewWithPlaceHolderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextViewWithPlaceHolderDemo
+//
+//  Created by 孙博弘 on 14/12/1.
+//  Copyright (c) 2014年 Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TextViewWithPlaceHolderDemoTests/TextViewWithPlaceHolderDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TextViewWithPlaceHolderDemoTests.swift
+//  TextViewWithPlaceHolderDemoTests
+//
+//  Created by 孙博弘 on 14/12/1.
+//  Copyright (c) 2014年 Sun. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TextViewWithPlaceHolderDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
